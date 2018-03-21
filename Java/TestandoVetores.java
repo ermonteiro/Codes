30c7720006c6177e8561e78c68a5bd0a75bcc9ee
ermonteiro <eduardarmonteiro@gmail.com>
2018-03-21 18:24:32 -0300

    Testando Novamente

---
+++ Java/TestandoVetores.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class TestandoVetores{
+
+    public static void main(String[] args) {
+        int[] numeros = new int[10];
+        //System.out.println(numeros[5]);
+        System.out.println(numeros);
+    }
+}
